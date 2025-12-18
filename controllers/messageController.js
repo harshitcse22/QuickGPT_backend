417f36df1228495e389a6230857a633e63da0688
Harshit <hktiwari980@gmail.com>
2025-12-18 20:31:53 +0530

    ai is integrated

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,10 +1,20 @@
 import Chat from "../models/Chat.js";
+import axios from "axios";
+import User from "../models/User.js";
+import imagekit from "../configs/imageKit.js";
+import openai from "../configs/openai.js";
 
 
 // Text-based AI Chat Message controller
 export const textMessageController = async (req, res)=>{
     try {
         const userId = req.user._id;
+
+         // check credits
+        if(req.user.credits < 1){
+            return res.json({success: false, message: " You dont have the enough credits to use this feature"})
+        }
+
         const {chatId, prompt} = req.body;
 
         const chat = await Chat.findOne({userId, _id: chatId})
@@ -12,7 +22,7 @@ export const textMessageController = async (req, res)=>{
        isImage: false})
 
        const {choices} = await openai.chat.completions.create({
-    model: "gemini-2.0-flash",
+    model: "gemini-2.0",
     messages: [
         {
             role: "user",
@@ -30,7 +40,7 @@ export const textMessageController = async (req, res)=>{
        await User.updateOne({_id: userId},{$inc: {credits: -1}})
 
     } catch (error) {
-        res.json({success: flase, message: error.message})
+        res.json({success: false, message: error})
     }
 }
 
@@ -47,8 +57,47 @@ export const imageMessageController = async (req, res)=>{
         const chat = await Chat.findOne({userId, _id: chatId})
 
         // Push user message
-        chat.messages.push({role: "user", content:prompt, timestamp:Date.now(), isImage: false})
+        chat.messages.push({
+            role: "user", 
+            content:prompt, 
+            timestamp:Date.now(), 
+            isImage: false});
+
+            //encode the prompt
+         const encodedPrompt = encodeURIComponent(prompt)
+
+         // construct Imagekit AI generation URL
+         const generatedImageUrl = `${process.env.IMAGEKIT_URL_ENDPOINT}/
+           ik-genimg-promt-${encodedPrompt}/quickgpt/${Date.now()}.png?tr=w-800,
+            h-800`;
+            
+            // Trigger generation by fetching from Imagekit
+             const aiImageResponse = await axios.get(generatedImageUrl, {responseType: "arraybuffer"});
+
+             // convert to Base64
+             const base64Image = `data:image/png;base64,${Buffer.from(aiImageResponse.data, "binary").toString("base64")}`;
+
+             // upload to imagekit media library
+             const uploadResponse = await imagekit.upload({
+                file: base64Image,
+                fileName: `${Date.now()}.png`,
+                folder: "quickgpt"
+             })
+
+             const reply = {role: 'assistant',
+                            content: uploadResponse.url,
+                            timestamp: Date.now(),
+                            isImage: true, 
+                             isPublished
+                            }
+                 res.json({success: true, reply})
+
+                 chat.messages.push(reply)
+                 await chat.save();
+
+                 await User.updateOne({_id: userId}, {$inc: {credits: -2}})
+
     } catch (error) {
-        
+        res.json({success:false, message: error.message})
     }
 }
